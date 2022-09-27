dea97925e87a41243b1ba2b076d1818cefa41b20
krystal lee <theekrystallee@gmail.com>
2022-09-27 02:38:58 -0700

    updated frontend

---
+++ my-app/constants/index.js
@@ -0,0 +1,66 @@
+export const ALLOWLIST_CONTRACT_ADDRESS = "0x3414608de0EF06aB26e0E4E3b347C9BF5B380B41";
+export const abi = [
+    {
+      "inputs": [
+        {
+          "internalType": "uint8",
+          "name": "_maxAllowlistedAddresses",
+          "type": "uint8"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "inputs": [],
+      "name": "addAddressToAllowlist",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "allowlistedAddresses",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "maxAllowlistedAddresses",
+      "outputs": [
+        {
+          "internalType": "uint8",
+          "name": "",
+          "type": "uint8"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "numAddressesAllowlisted",
+      "outputs": [
+        {
+          "internalType": "uint8",
+          "name": "",
+          "type": "uint8"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ];
--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,69 +1,217 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Web3Modal from "web3modal";
+import { providers, Contract } from "ethers";
+import { useEffect, useRef, useState } from "react";
+import { ALLOWLIST_CONTRACT_ADDRESS, abi } from "../constants";
+     
 export default function Home() {
+  // walletConnected keep track of whether the user's wallet is connected or not
+  const [walletConnected, setWalletConnected] = useState(false);
+  // joinedAllowlist keeps track of whether the current metamask address has joined the Allowlist or not
+  const [joinedAllowlist, setJoinedAllowlist] = useState(false);
+  // loading is set to true when we are waiting for a transaction to get mined
+  const [loading, setLoading] = useState(false);
+  // numberOfAllowlisted counts and tracks the number of addresses's allowlisted
+  const [numberOfAllowlisted, setNumberOfAllowlisted] = useState(0);
+  // create a reference to the Web3 Modal (used for connecting to Metamask) which persists as long as the page is open
+  const web3ModalRef = useRef();
+
+  /**
+   * returns a Provider or Signer object representing the Ethereum RPC with or without the
+   * signing capabilities of metamask attached
+   *
+   * `Provider` is needed to interact with the blockchain - reading transactions, reading balances, reading state, etc.
+   *
+   * `Signer` is a special type of Provider used in case a `write` transaction needs to be made to the blockchain, which involves the connected account
+   * needing to make a digital signature to authorize the transaction being sent. Metamask exposes a Signer API to allow your website to
+   * request signatures from the user using Signer functions.
+   *
+   * @param {*} needSigner - true if you need the signer, default false otherwise
+   */
+  const getProviderOrSigner = async (needSigner = false) => {
+    // connect to Metamask
+    // since we store `web3Modal` as a reference, we need to access the current value to get access to the underlying object
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+
+    // ff user is not connected to the Mumbai network, throw an error
+    const { chainId } = await web3Provider.getNetwork();
+    if (chainId !== 4) {
+      window.alert("Change the network to MUMBAI");
+      // throw new error("Change network to MUMBAI!");
+    }
+
+    if (needSigner) {
+      const signer = web3Provider.getSigner();
+      return signer;
+    }
+    return web3Provider;
+  };
+
+  /**
+   * addAddressToAllowlist: adds the current connected address to the allowlist
+   */
+  const addAddressToAllowlist = async () => {
+    try {
+      // we need a Signer here since this is a 'write' transaction.
+      const signer = await getProviderOrSigner(true);
+      // create a new instance of the Contract with a Signer, which allows
+      const AllowlistContract = new Contract(
+        ALLOWLIST_CONTRACT_ADDRESS,
+        abi,
+        signer
+      );
+      // call the addAddressToAllowlist from the contract
+      const tx = await AllowlistContract.addAddressToAllowlist();
+      setLoading(true);
+      // wait for the transaction to get mined
+      await tx.wait();
+      setLoading(false);
+      // get the updated number of addresses in the allowlist
+      await getNumberOfAllowlisted();
+      setJoinedAllowlist(true);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  /**
+   * getNumberOfAllowlisted:  gets the count number of allowlisted addresses
+   */
+  const getNumberOfAllowlisted = async () => {
+    try {
+      // get the provider from web3Modal (MM)
+      // no need for the Signer here, as we are only reading state from the blockchain
+      const provider = await getProviderOrSigner();
+      // we connect to the Contract using a Provider, so we will only
+      // have read-only access to the Contract
+      const AllowlistContract = new Contract(
+        ALLOWLIST_CONTRACT_ADDRESS,
+        abi,
+        provider
+      );
+      // calls the numAddressesAllowlisted from the contract
+      const _numberOfAllowlisted = await allowlistContract.numAddressesAllowlisted();
+      setNumberOfAllowlisted(_numberOfAllowlisted);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  /**
+   * checkIfAddressInAllowlist: checks if the address is in list of allowlisted addresses
+   */
+  const checkIfAddressInAllowlist = async () => {
+    try {
+      // we will need the signer later to get the user's address
+      const signer = await getProviderOrSigner(true);
+      const allowlistContract = new Contract(
+        ALLOWLIST_CONTRACT_ADDRESS,
+        abi,
+        signer
+      );
+      // get the address associated to the signer connected to MM
+      const address = await signer.getAddress();
+      // call the AllowlistedAddresses from the contract
+      const _joinedAllowlist = await allowlistContract.allowlistedAddresses(
+        address
+      );
+      setJoinedAllowlist(_joinedAllowlist);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  /*
+    connectWallet: connects the MetaMask wallet
+  */
+  const connectWallet = async () => {
+    try {
+      // get the provider from web3Modal (MetaMask)
+      // Wwhen used for the first time, it prompts the user to connect wallet
+      await getProviderOrSigner();
+      setWalletConnected(true);
+
+      checkIfAddressInAllowlist();
+      getNumberOfAllowlisted();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  /*
+    renderButton: returns state-based button
+  */
+  const renderButton = () => {
+    if (walletConnected) {
+      if (joinedAllowlist) {
+        return (
+          <div className={styles.description}>
+            Thanks for joining the allowlist!
+          </div>
+        );
+      } else if (loading) {
+        return <button className={styles.button}>Loading...</button>;
+      } else {
+        return (
+          <button onClick={addAddressToAllowlist} className={styles.button}>
+            Join DevDate Allowlist
+          </button>
+        );
+      }
+    } else {
+      return (
+        <button onClick={connectWallet} className={styles.button}>
+          Connect your wallet!
+        </button>
+      );
+    }
+  };
+
+  // useEffects are used to react to changes in state of the website
+  // the array at the end of function call represents what state changes will trigger this effect
+  // in this case, whenever the value of `walletConnected` changes - this effect will be called
+  useEffect(() => {
+    // if wallet is not connected, create a new instance of Web3Modal and connect the MetaMask wallet
+    if (!walletConnected) {
+      // assign the Web3Modal class to the reference object by setting it's current value
+      // current value is persisted throughout as long as this page is open
+      web3ModalRef.current = new Web3Modal({
+        network: "mumbai",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+      connectWallet();
+    }
+  }, [walletConnected]);
+
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Generated by create next app" />
+        <title>Allowlist DApp</title>
+        <meta name="description" content="allowlist-Dapp" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className={styles.main}>
+        <div>
+          <h1 className={styles.title}>Welcome to Dev N Date!</h1>
+          <div className={styles.description}>
+            An NFT collection for developers interested in D_D dating show &#10084;
+          </div>
+          <div className={styles.description}>
+            {numberOfAllowlisted} have already joined the DevDate allowlist
+          </div>
+          {renderButton()}
         </div>
-      </main>
+        <div>
+          <img className={styles.image} src="./dev-date.svg" />
+        </div>
+      </div>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        Made with &#10084; by D_D
       </footer>
     </div>
-  )
+  );
 }
